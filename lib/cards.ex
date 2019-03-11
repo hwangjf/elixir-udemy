af1e731d2ba83b416c9115dee335af3477e8f309
github username <jeffreyhwang90@gmail.com>
2019-03-10 22:45:55 -0400

    section 2.1 pattern matching

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,7 +1,25 @@
 defmodule Cards do
 
-  def hello do
-    'hi there!'
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for value <- values, suit <- suits do
+      "#{value} of #{suit}"
+    end
+    
   end
-  
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
 end
